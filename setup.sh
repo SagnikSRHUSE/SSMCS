1675a044c1430bc65e3f543b6e120fc5bfb14014
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2019-09-30 17:26:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
-mkdir /opt
+if [ ! -d /opt ]; then
+  mkdir /opt
+fi
+
 if [ ! -f /opt/monitor.sh ]; then
-  wget https://raw.githubusercontent.com/SagnikSRHUSE/SSMCS/master/monitor.sh -O /opt/monitor.sh
+  wget https://raw.githubusercontent.com/SagnikSRHUSE/SSMCS/master/monitor.sh -O /opt/monitor.sh && chmod +x /opt/monitor.sh
 else
   echo "/opt/monitor.sh already existsm, are you sure you want to continue?"
   read RESPONSE
   if [[ "${RESPONSE}" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
       exit 0
   fi
+fi
+
 crontab -l | { cat; echo "* * * * * /opt/monitor.sh $1"; } | crontab -
